b5d03377ed61f91da4b9b440bf912faef3e7f3f8
Pedro Nascimento <pedro@krooze.com.br>
2023-06-25 18:22:31 -0300

    Implementa retorno de erro na tela

--- FidelityCard.Presentation/Controllers/ProductController.cs
+++ FidelityCard.Presentation/Controllers/ProductController.cs
@@ -8,13 +8,13 @@ namespace FidelityCard.Presentation.Controllers;
 
 public class ProductsController : Controller
 {
-    private readonly IProductService _ProductService;
-    private readonly ICompanyService _CompanyService;
+	private readonly IProductService _ProductService;
+	private readonly ICompanyService _CompanyService;
 
-    public ProductsController(IProductService ProductService, ICompanyService CompanyService)
-    {
+	public ProductsController(IProductService ProductService, ICompanyService CompanyService)
+	{
 		_ProductService = ProductService;
-        _CompanyService = CompanyService;
+		_CompanyService = CompanyService;
 	}
 
 	public ActionResult Index()
@@ -48,9 +48,12 @@ public class ProductsController : Controller
 
 			return RedirectToAction(nameof(Index));
 		}
-		catch
+		catch (Exception ex)
 		{
-			return View();
+			ViewData["Error"] = ex.Message;
+			var companies = _CompanyService.GetAll().ToArray();
+			ViewBag.Companies = companies;
+			return View(dto);
 		}
 	}
 
